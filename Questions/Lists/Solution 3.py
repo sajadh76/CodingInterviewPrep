c98fc4dbebe92e3e283a2d07338444091f76deb2
Sajad Hamzenejadi <40471014+sajadh76@users.noreply.github.com>
2024-04-30 07:30:42 +0200

    Solution 3.py

---
+++ Questions/Lists/Solution 3.py
@@ -0,0 +1,16 @@
+first_list = [5, 3, 0, 7, 7, 7, 7, 8, 0, 10]
+my_dict = {}
+max_value = 0
+
+for num in first_list:
+    if num in my_dict:
+        my_dict[num] += 1
+    else:
+        my_dict[num] = 1
+
+    if my_dict[num] > max_value:
+        max_value = my_dict[num]
+
+for key, value in my_dict.items():
+    if value == max_value:
+        print(key)
